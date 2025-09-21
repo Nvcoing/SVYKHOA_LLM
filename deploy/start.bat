d3aa2b0ab5e16b218c63944cb173aa3e1e7e6b51
Nvcoding <166248158+Nvcoing@users.noreply.github.com>
2025-09-21 15:32:51 +0700

    Auto commit

--- deploy/start.bat
+++ deploy/start.bat
@@ -7,8 +7,8 @@ REM Chạy tunnel trong CMD mới
 start "" "D:\Git\Git\git-bash.exe" --cd="E:\job\SVYKHOA_LLM\SVYKHOA_LLM\deploy" -c "./start_tunnel.sh"
 
 
-REM Đợi 5 giây để 2 script trên chạy
-timeout /t 5 /nobreak >nul
+REM Đợi 10 giây để 2 script trên chạy
+timeout /t 10 /nobreak >nul
 
 REM Thực hiện git push
 git add .
