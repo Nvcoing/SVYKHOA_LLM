b0f68a10c479956a3efab97b39ba3002da5cea44
Nvcoding <166248158+Nvcoing@users.noreply.github.com>
2025-09-22 15:37:21 +0700

    fix parse

--- deploy/handlers/response.py
+++ deploy/handlers/response.py
@@ -1,19 +1,19 @@
 from agent_server.generate import generate_stream
 from handlers.pasre_tool import extract_tool_json_from_response as parse
+import json
 
 def handle_generate_request(model, tokenizer, device, prompt: str, labels: str = ""):
     print(f"Received prompt: {prompt}")
 
-    # tao generator
     stream = generate_stream(model, tokenizer, device, prompt, labels)
-
     buffer = ""
     for chunk in stream:
         buffer += chunk
-        # Neu gap the <tool> thi dung
         if "<tool>" in buffer:
             tool_json = parse(buffer)
-            yield {"event": "tool", "tool": tool_json}
+            # Gui JSON event cho client, da duoc dump thanh string
+            yield json.dumps({"event": "tool", "tool": tool_json}, ensure_ascii=False)
             return
         else:
-            yield {"event": "text", "data": chunk}
+            # Gui chunk text thong thuong
+            yield chunk
