7c1f77ad14e1711d78fe11acfad94c1df13274f3
Nvcoding <166248158+Nvcoing@users.noreply.github.com>
2025-09-25 11:36:39 +0700

    Auto deploy

--- deploy/start.bat
+++ deploy/start.bat
@@ -6,11 +6,12 @@ start "" "..\tools\Git\git-bash.exe" --cd=".\deploy" -c "./start_server.sh"
 REM Chạy tunnel trong CMD mới
 start "" "..\tools\Git\git-bash.exe" --cd=".\deploy" -c "./start_tunnel.sh"
 
+REM Đợi 10 giây để 2 script trên chạy
+timeout /t 20 /nobreak >nul
+
 REM Chạy html-obfuscator trong Git Bash mới
 start "" "..\tools\Git\git-bash.exe" -c "html-obfuscator chatsvy.html index.html; exec bash"
 
-REM Đợi 10 giây để 2 script trên chạy
-timeout /t 10 /nobreak >nul
 
 REM Thực hiện git push
 git checkout dev
