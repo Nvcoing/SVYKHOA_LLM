2c2154706f4d9111ef1ea5ac451e676bae6eba22
Nvcoding <166248158+Nvcoing@users.noreply.github.com>
2025-10-03 11:27:30 +0700

    fix

--- deploy/handlers/prompt.py
+++ deploy/handlers/prompt.py
@@ -15,24 +15,15 @@ def build_prompt(prompt: str, labels: str = "",auguments_local=None, auguments_o
         Symptom = auguments_local["Symptom"]
         prompts = (
                 # f"<|begin_of_text|>\n{Intruction}\nCâu hỏi cần trả lời:{prompt.strip()}\nHãy trình bày câu hỏi dựa vào nội dung sau:\n**Đoạn trích ngắn**:{snippet}\n**Nội dung chính**:{highlight}\nHãy trình bày câu hỏi theo dạng dưới đây nhưng không được lấy theo nội dung:\n**Chuẩn đoán:**{Diagnosis}\n**Triệu chứng:**{Symptom}\n<label>{labels.strip()}</label>\n"
-                f"<|begin_of_text|>\n{Intruction}\n",
-                # f"**Hãy trả lời câu hỏi sau**:{prompt.strip()}\n",
-                f"Hãy trả lời câu hỏi **{prompt.strip()}** dựa vào nội dung sau:\n* **Chuẩn đoán**:\n{Diagnosis}\n",
-                f"* **Triệu chứng**:\n{Symptom}\n",
-                f"Lưu ý: Bạn không được trả lời theo nội dung trên mà phải dựa vào kiến thức y khoa của bạn để trả lời đúng nội dung câu hỏi.\n",
-                f"<label>{labels.strip()}</label>\n"
+                f"<|begin_of_text|>\n{Intruction}\nHãy trả lời câu hỏi **{prompt.strip()}** dựa vào nội dung sau:\n* **Chuẩn đoán**:\n{Diagnosis}\n* **Triệu chứng**:\n{Symptom}\nLưu ý: Bạn không được trả lời theo nội dung trên mà phải dựa vào kiến thức y khoa của bạn để trả lời đúng nội dung câu hỏi.\n<label>{labels.strip()}</label>\n",
         )
     else:
         Intruction = auguments_local["Intruction"]
         Answer = auguments_local["Answer"]
         prompts = (
                 # f"<|begin_of_text|>\n{Intruction}\nCâu hỏi cần trả lời:{prompt.strip()}\nHãy trình bày câu hỏi dựa vào nội dung sau:\n**Đoạn trích ngắn**:{snippet}\n**Nội dung chính**:{highlight}\nHãy trình bày câu hỏi theo dạng dưới đây nhưng không được lấy theo nội dung:\n{Answer}\n<label>{labels.strip()}</label>\n"
-                                f"<|begin_of_text|>\n{Intruction}\n",
-                                f"Hãy trả lời câu hỏi **{prompt.strip()}** dựa vào nội dung sau:\n",
-                                # f"Hãy trình bày câu hỏi dựa vào nội dung sau:\n",
-                                f"* **Câu trả lời**:\n{Answer}\n",
-                                f"Lưu ý: Bạn không được trả lời theo nội dung trên mà phải dựa vào kiến thức y khoa của bạn để trả lời đúng nội dung câu hỏi.\n",
-                                f"<label>{labels.strip()}</label>\n"
+                                f"<|begin_of_text|>\n{Intruction}\nHãy trả lời câu hỏi **{prompt.strip()}** dựa vào nội dung sau:\n* **Câu trả lời**:\n{Answer}\nLưu ý: Bạn không được trả lời theo nội dung trên mà phải dựa vào kiến thức y khoa của bạn để trả lời đúng nội dung câu hỏi.\n<label>{labels.strip()}</label>\n",
+
         )
     print("Generated Prompt:", prompts)
     return prompts
