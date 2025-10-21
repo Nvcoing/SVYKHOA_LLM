11606ac983ac232bb2f35673ddc14c5dfa467f25
Nvcoding <166248158+Nvcoing@users.noreply.github.com>
2025-10-21 23:25:36 +0700

    train

--- train/trainer.py
+++ train/trainer.py
@@ -61,8 +61,7 @@ def get_trainer(model, tokenizer, dataset, repo_id="NV9523/CHAT_SVY", hf_token=N
     dataset_tokenized = dataset.map(
         tokenize_batch,
         batched=True,
-        batch_size=32,              
-        num_proc=1,
+        batch_size=8,              
         remove_columns=dataset.column_names
     )
 
