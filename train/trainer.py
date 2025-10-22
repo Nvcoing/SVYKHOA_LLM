e9bdd94c7fc43bdd1419299003120070ddb9a79e
Nvcoding <166248158+Nvcoing@users.noreply.github.com>
2025-10-22 12:01:00 +0700

    train

--- train/trainer.py
+++ train/trainer.py
@@ -77,7 +77,7 @@ def get_trainer(model, tokenizer, dataset, repo_id="NV9523/CHAT_SVY", hf_token=N
     training_args = TrainingArguments(
         output_dir="SVYKHOA_Chatbox",
         per_device_train_batch_size=1,
-        gradient_accumulation_steps=0,
+        gradient_accumulation_steps=1,
         num_train_epochs=1,
         learning_rate=1e-3,
         fp16=True,
